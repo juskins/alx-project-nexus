ee84cbd6f3d7eb714df3995ab3bb8eb6d686f3cb
juskins <omojuwababatunde1@gmail.com>
2026-02-06 10:23:25 +0100

    feat: add netlify app to allowed cors origins

--- campus-connect/next.config.ts
+++ campus-connect/next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  // output: 'export',
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
