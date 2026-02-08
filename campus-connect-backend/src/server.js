0956abf4594cfd93299c346587284649ce2a1969
juskins <omojuwababatunde1@gmail.com>
2026-02-08 01:59:55 +0100

    fix cors issue from :5000

--- campus-connect-backend/src/server.js
+++ campus-connect-backend/src/server.js
@@ -36,6 +36,7 @@ app.use(
       origin: [
          process.env.FRONTEND_URL,
          'http://localhost:3000',
+         'http://localhost:5000',
          'https://campus-jobs.netlify.app'
       ].filter(Boolean),
       credentials: true,
